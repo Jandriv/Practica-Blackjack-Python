5e6e303c745849cba62298fa3652e66b7b73d2d7
Jandriv <90726642+Jandriv@users.noreply.github.com>
2024-03-13 19:41:24 +0100

    Test 2

--- Practica.py
+++ Practica.py
@@ -1,3 +1,13 @@
-from FichExterno import CartaBase, Estrategia, Mazo
+import FichExterno
 
-FichExterno.CartaBase()
+a = FichExterno.CartaBase(9)
+print(a.ind)
+print(a.valor)
+
+def main():
+    print("BLACKJACK - PARADIGMAS DE PROGRAMACION 2023/2024")
+    print("¿Modo de ejecución? [J]uego [A]nálisis: ", end="")
+    respuesta = input()
+
+main()
+
