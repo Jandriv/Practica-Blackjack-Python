6dadf1c9bf1214155c3c7f92a4866545ba91fc1c
Jandriv <90726642+Jandriv@users.noreply.github.com>
2024-03-13 19:54:10 +0100

    Test inicio

--- Practica.py
+++ Practica.py
@@ -4,10 +4,20 @@ a = FichExterno.CartaBase(9)
 print(a.ind)
 print(a.valor)
 
+
 def main():
     print("BLACKJACK - PARADIGMAS DE PROGRAMACION 2023/2024")
-    print("¿Modo de ejecución? [J]uego [A]nálisis: ", end="")
-    respuesta = input()
-
+    respuesta = "Null"
+    print(respuesta)
+    while ((respuesta != "J") & (respuesta != "A")):
+        print("¿Modo de ejecución? [J]uego [A]nálisis: ", end="")
+        respuesta = input().upper()
+        if respuesta == "J":
+            Analisis = False
+        elif respuesta == "A":
+            Analisis = True
+        else:
+            print("Respuesta no válida")
+        
 main()
 
